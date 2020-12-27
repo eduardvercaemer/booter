85a6c0f8a4c0b3f555b7227f55eb0eef52294efa
Eduard Vercaemer <vercaemereduard@gmail.com>
2020-12-27 01:39:52 -0600

    initial work on graphic mode

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -1,18 +1,12 @@
 #include "system.h"
 #include "vga.h"
-#include "mem.h"
 
 void kmain()
 {
-    move(800);
-    puts("hello, kernel", 0x03);
-
-    move(960);
-    puts("kernel start : ", 0x03);
-    puth(kstart, 0x03);
-    move(1040);
-    puts("kernel end :   ", 0x03);
-    puth(kend, 0x03);
+    //move(800);
+    //puts("hello, vga", 0x03);
+    
+    init_graphics();
 
     hang();
 }
--- kernel/src/vga.h
+++ kernel/src/vga.h
@@ -1,7 +1,16 @@
+#ifndef KER_VGA_H
+#define KER_VGA_H
 #include "types.h"
+
+/* vga.c */
+extern void init_graphics();
+extern void pixel(u16 x, u16 y, u8 col);
+extern void clear();
 
 /* vga_text.c */
 extern void move(int pos);
 extern void putc(char c, char attr);
 extern void puts(const char *str, char attr);
 extern void puth(u32 val, char attr);
+
+#endif
